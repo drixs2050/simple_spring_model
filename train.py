b9b34449e20a5f47457ebd9bb613ead730d077e7
drixs2050 <39656972+drixs2050@users.noreply.github.com>
2021-08-26 10:44:27 -0400

    finished ml model

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 import torch.optim as optim
 from trajectory_model import *
 import pandas as pd
+import copy
 
 
 def my_MSEloss(a, b):
@@ -10,6 +11,8 @@ def my_MSEloss(a, b):
 
 def train_pos(posModel, t_tensor, pos_tensor, num_epoch, loss_function=my_MSEloss):
 	optimizer = optim.Adam(posModel.parameters(), lr=0.0001)
+	best_loss = float('inf')
+	best_weights = copy.deepcopy(posModel.state_dict())
 	for epoch in range(num_epoch):
 		pred = posModel(t_tensor)
 		loss = loss_function(pred, pos_tensor)
@@ -18,18 +21,28 @@ def train_pos(posModel, t_tensor, pos_tensor, num_epoch, loss_function=my_MSElos
 			print('pos_loss: {}'.format(loss))
 		loss.backward()
 		optimizer.step()
+		if loss < best_loss:
+			best_loss = loss
+			best_weights = copy.deepcopy(posModel.state_dict())
+	torch.save(best_weights, "weights/" + "pos_mod_weights.pth")
 
 
 def train_ang_vels(angModel, t_tensor, ang_vels_tensor, num_epoch, loss_function=my_MSEloss):
-	optimizer2 = optim.Adam(angModel.parameters(), lr=0.0001)
+	optimizer = optim.Adam(angModel.parameters(), lr=0.0001)
+	best_loss = float('inf')
+	best_weights = copy.deepcopy(angModel.state_dict())
 	for epoch in range(num_epoch):
 		pred = angModel(t_tensor)
 		loss = loss_function(pred, ang_vels_tensor)
-		optimizer2.zero_grad()
+		optimizer.zero_grad()
 		if epoch % 100 == 0:
 			print('ang_loss: {}'.format(loss))
 		loss.backward()
-		optimizer2.step()
+		optimizer.step()
+		if loss < best_loss:
+			best_loss = loss
+			best_weights = copy.deepcopy(angModel.state_dict())
+	torch.save(best_weights, "weights/"+"ang_mod_weights.pth")
 
 
 if __name__ == "__main__":
@@ -45,7 +58,10 @@ if __name__ == "__main__":
 	t_tensor = pos_data[:, 0].reshape(len(pos_data), 1).to(device).float()
 	pos_tensor = pos_data[:, 1:].to(device).float()
 	ang_vels_tensor = ang_vels_data[:, 1:].to(device).float()
-	num_epoch = 10000
+	num_epoch = 101
 	posModel = TrajModNetwork(pos_tensor.shape[1]).to(device)
 	angModel = AngleTrajNet(ang_vels_tensor.shape[1]).to(device)
+	train_pos(posModel, t_tensor, pos_tensor, num_epoch)
 	train_ang_vels(angModel, t_tensor, ang_vels_tensor, num_epoch)
+	posModel.load_state_dict(torch.load("weights/pos_mod_weights.pth"))
+	angModel.load_state_dict(torch.load("weights/ang_mod_weights.pth"))
